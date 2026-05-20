42bdd6718812bdf19dcd12c858ad56083f78e598
M1sk0x <mziacik2@gmail.com>
2026-05-20 09:11:42 +0200

    pridany pekny close button ku každemu oknu

--- MacroMeter/LoginWindow.cs
+++ MacroMeter/LoginWindow.cs
@@ -10,7 +10,10 @@ namespace MacroMeter
             Database.Initialize();
 
         }
-
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Zatvorí okno prihlásenia (a celú aplikáciu)
+        }   
         private void Login_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrWhiteSpace(EmailBox.Text) ||
